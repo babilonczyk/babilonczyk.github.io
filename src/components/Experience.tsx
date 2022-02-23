688aa4d576bdd5cc8040b0f2b4d6c5a4e5744799
babilonczyk <piotrzkowski.jan@gmail.com>
2022-02-23 22:39:23 +0100

    Fix mobile version

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -56,7 +56,7 @@ const Experience: React.FC  = () => {
                         </TabPanel>
                         <TabPanel className="experience-box-content-panel">
                             <h5>2020</h5>
-                            <h4>University Time</h4>
+                            <h4>University Time</h4> 
                             <p>
                                 2019-2020 were the years when I've started getting interested in web technologies. Especially Ruby 
                                 and Ruby on Rails.
